c5365d8c43d4d183404c28025d6fd5ae2b6448ea
uzzal111 <uzzaldatta310@gmail.com>
2025-12-18 00:19:32 +0600

    error solved

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import HomeNavbar from '@/components/ui/home/Navbar';
-import HomeFotter from '@/components/ui/home/Fotter';
-import MobileFooterNav from '@/components/ui/auth/foternavbar';
 const HomeLayout = ({
 	children,
 }: Readonly<{
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import HomeLayout from './(home)/layout';
 import BannerSlider from '@/components/ui/home/dashboard';
-import UserCountdown from '@/components/ui/home/UserGrowth';
-import Testimonials from '@/components/ui/home/Testimonials';
-import Benefits from '@/components/ui/home/Benefits';
-import FAQ from '@/components/ui/home/Faq';
-import Partners from '@/components/ui/home/Partners';
-import Conference from '@/components/ui/home/confernce';
-import TaskRecordComponent from '@/components/ui/home/taskrecord';
+
 import Crisp from '@/components/ui/auth/crisp';
 import CompanyOffersTicker from '@/components/ui/home/welcomenotice';
 
